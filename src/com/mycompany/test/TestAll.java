526534144b34bf065b3217dbc00d284d5659efb3
Loandt1 <loandt.uet@gmail.com>
2018-05-12 23:59:54 +0700

    update code

---
+++ src/com/mycompany/test/TestAll.java
@@ -0,0 +1,17 @@
+package com.mycompany.test;
+
+import com.mycompany.generator.AsenAPIDriver;
+
+public class TestAll {
+	// "/Users/loandinh/AnypointStudio/workspace/muleesbbegin"
+		public static void main(String[] args) {
+			String path = "/Users/loandinh/Desktop/luanvan/Generator/muleesbbegin";
+			AsenAPIDriver a = AsenAPIDriver.newInstance();
+			a.init(path);
+			try {
+				a.generate();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+}
