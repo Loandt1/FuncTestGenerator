f417ece8742a0b5dc31d47dc447b015c1c635717
Loandt1 <loandt.uet@gmail.com>
2018-05-16 22:45:49 +0700

    xuat thu vien

--- src/com/mycompany/test/TestAll.java
+++ src/com/mycompany/test/TestAll.java
@@ -5,7 +5,7 @@ import com.mycompany.generator.AsenAPIDriver;
 public class TestAll {
 	// "/Users/loandinh/AnypointStudio/workspace/muleesbbegin"
 		public static void main(String[] args) {
-			String path = "/Users/loandinh/Desktop/luanvan/Generator/muleesbbegin";
+			String path = "/Users/loandinh/Desktop/luanvan/Generator/MuleESBBeginner/TestMuleESB";
 			AsenAPIDriver a = AsenAPIDriver.newInstance();
 			a.init(path);
 			try {
